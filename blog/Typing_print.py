1dc82bac03e18a8f96bdef106a5c5881b8e6a2b6
tesSer16 <80532804+tesSer16@users.noreply.github.com>
2021-10-09 05:27:39 +0900

    만능문장2 적용, kRtype에 맞게 변경

---
+++ blog/Typing_print.py
@@ -0,0 +1,14 @@
+import KRtype as kt
+import time
+
+
+def Tprint(string):
+    disassembled_string = kt.disassemble(string)
+    for i in range(len(disassembled_string)):
+        print("\r" + kt.assemble(list(disassembled_string[:i+1])), end="")
+        time.sleep(0.2)
+
+
+if __name__ == "__main__":
+    Tprint("ㅘ! 'ㄲ', 'ㅠ', 'ㄺ'의 합성인 굴국(뀱) 먹으러 ㄱㄱ싱~")
+    input()
